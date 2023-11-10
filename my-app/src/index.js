8fcee126097be834fd70160d8dd700081356b9b2
Hwang <hku2002@hanmail.net>
2023-11-10 22:37:05 +0900

    feat: 시간을 나타내보기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import Library from "./chapter03/Library";
+import Clock from "./chapter04/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+setInterval(() => {
+    root.render(
+        <React.StrictMode>
+            <Clock />
+        </React.StrictMode>
+    );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
