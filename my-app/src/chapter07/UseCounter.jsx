5b83728f56c2bb9c81402c26e96788790c6fe246
Hwang <hku2002@hanmail.net>
2023-11-11 19:23:48 +0900

    refactor: 매계변수명 수정

--- my-app/src/chapter07/UseCounter.jsx
+++ my-app/src/chapter07/UseCounter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
-function useCounter(initValue) {
-    const [count, setCount] = useState(initValue);
+function useCounter(initialValue) {
+    const [count, setCount] = useState(initialValue);
 
     const increaseCount = () => setCount(count => count + 1);
     const decreaseCount = () => setCount(count => Math.max(count - 1, 0));
