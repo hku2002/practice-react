e95a2d32e2993f852192566b6cba480bb6eeaead
Hwang <hku2002@hanmail.net>
2023-11-11 14:38:08 +0900

    feat: state 연습 코드 추가

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import CommentList from "./chapter05/CommentList";
+import NotificationList from "./chapter06/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <CommentList />
+        <NotificationList />
     </React.StrictMode>
 );
 
