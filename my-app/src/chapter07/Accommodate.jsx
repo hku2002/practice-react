453e7c4cb3f082377ec07a82c6e377b2bbad6a64
Hwang <hku2002@hanmail.net>
2023-11-11 19:43:44 +0900

    feat: 0명이면 퇴장 버튼 비활성화 기능 추가

--- my-app/src/chapter07/Accommodate.jsx
+++ my-app/src/chapter07/Accommodate.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import useCounter from "./UseCounter"
 
 const MAX_CAPACITY = 10;
+const MIN_COUNT = 0;
 
 function Accommodate(props) {
     const [isFull, setIsFull] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(true);
     const [count, increaseCount, decreaseCount] = useCounter(0);
 
     useEffect(() => {
@@ -16,6 +18,7 @@ function Accommodate(props) {
     useEffect(() => {
         console.log("## 의존성 배열이 존재하는 useEffect ##");
         setIsFull(count >= MAX_CAPACITY);
+        setIsEmpty(count <= MIN_COUNT);
         console.log(`Current count value: ${count}`);
     }, [count]);
 
@@ -24,7 +27,7 @@ function Accommodate(props) {
             <p>{`총 ${count}명을 수용했습니다.`}</p>
 
             <button onClick={increaseCount} disabled={isFull}>입장</button>
-            <button onClick={decreaseCount}>퇴장</button>
+            <button onClick={decreaseCount} disabled={isEmpty}>퇴장</button>
 
             {isFull && <p style={{ color: "red" }}>정원이 가득찼습니다.</p>}
         </div>
