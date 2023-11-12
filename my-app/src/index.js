4ee4c98ea8451245303682e6594de15f45517ec3
Hwang <hku2002@hanmail.net>
2023-11-12 12:17:21 +0900

    fix: 오타 수정

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import ConfirmBotton from "./chapter08/ConfirmBotton";
+import ConfirmButton from "./chapter08/ConfirmButton";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ConfirmBotton />
+        <ConfirmButton />
     </React.StrictMode>
 );
 
