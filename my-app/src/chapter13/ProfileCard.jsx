72c1f8682f220d43f71f7bf9e8f11468fe92005a
Hwang <hku2002@hanmail.net>
2023-11-17 21:46:43 +0900

    feat: 프로필 카드 추가

---
+++ my-app/src/chapter13/ProfileCard.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card";
+
+export default function ProfileCard(props) {
+    return (
+        <Card title="Connie" backgroundColor="#4ea04e">
+            <p>안녕하세요. Connie 입니다.</p>
+            <p>리액트 공부중 입니다.</p>
+        </Card>
+    );
+}
