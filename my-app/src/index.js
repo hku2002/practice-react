70cc91261bf0f04f0bd1257be527a44540f8283d
hku2002 <hku2002@hanmail.net>
2023-11-20 22:35:05 +0900

    fix: 다크모드 테스트 해보기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import ProfileCard from "./chapter13/ProfileCard";
+import DarkMode from "./chapter14/DarkMode";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ProfileCard />
+        <DarkMode />
     </React.StrictMode>
 );
 
