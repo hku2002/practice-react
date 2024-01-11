2688741de4411dd4c277df260f21a58b02b03727
hku2002 <hku2002@hanmail.net>
2024-01-11 23:57:10 +0900

    config: db 설정 추가

---
+++ todo-backend/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO todo (title, date) VALUES ('첫째날 할일', '2024-01-01 00:00:01');
+INSERT INTO todo (title, date) VALUES ('둘째날 할일', '2024-01-02 00:00:01');
+INSERT INTO todo (title, date) VALUES ('셋째날 할일', '2024-01-03 00:00:01');
+
+INSERT INTO task (task, todo_id) VALUES ('Complete homework', 1);
+INSERT INTO task (task, todo_id) VALUES ('Buy groceries', 1);
+INSERT INTO task (task, todo_id) VALUES ('Exercise', 1);
+
+INSERT INTO task (task, todo_id) VALUES ('Coding', 2);
+INSERT INTO task (task, todo_id) VALUES ('Study', 2);
+INSERT INTO task (task, todo_id) VALUES ('Exercise', 2);
+
+INSERT INTO task (task, todo_id) VALUES ('Go to work', 3);
+INSERT INTO task (task, todo_id) VALUES ('play game', 3);
+INSERT INTO task (task, todo_id) VALUES ('meet friends', 3);
