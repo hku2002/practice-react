801fbd413e44047fad8235b243d99d94e4e5a7c3
hku2002 <hku2002@hanmail.net>
2024-01-11 23:59:32 +0900

    feat: date 추가

--- todo-backend/src/main/java/kr/co/todo/api/dto/TodoResponseDto.java
+++ todo-backend/src/main/java/kr/co/todo/api/dto/TodoResponseDto.java
@@ -4,6 +4,7 @@ import kr.co.todo.api.domain.entity.Task;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -11,12 +12,14 @@ public class TodoResponseDto {
 
     private Long id;
     private String title;
+    private LocalDateTime date;
     private List<TaskResponseDto> tasks;
 
     @Builder
-    public TodoResponseDto(Long id, String title, List<TaskResponseDto> tasks) {
+    public TodoResponseDto(Long id, String title, LocalDateTime date, List<TaskResponseDto> tasks) {
         this.id = id;
         this.title = title;
+        this.date = date;
         this.tasks = tasks;
     }
 
@@ -27,6 +30,7 @@ public class TodoResponseDto {
         return TodoResponseDto.builder()
                 .id(tasks.get(0).getTodo().getId())
                 .title(tasks.get(0).getTodo().getTitle())
+                .date(tasks.get(0).getTodo().getDate())
                 .tasks(TaskResponseDto.from(tasks))
                 .build();
     }
