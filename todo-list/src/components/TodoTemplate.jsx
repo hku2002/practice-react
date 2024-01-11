d178ed8178344cfb31aa8f658faf0204bfc9fd0e
hku2002 <hku2002@hanmail.net>
2024-01-11 22:42:10 +0900

    fix: 한국식 날짜로 변경

--- todo-list/src/components/TodoTemplate.jsx
+++ todo-list/src/components/TodoTemplate.jsx
@@ -100,7 +100,7 @@ const TodoTemplate = (props) => {
     const getTodayDate = () => {
         const today = new Date();
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        return today.toLocaleDateString('en-US', options);
+        return today.toLocaleDateString('ko-KR', options);
     };
 
     return (
