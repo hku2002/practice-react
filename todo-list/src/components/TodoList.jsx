2ea415ea3f99a416317f4349988d6aeb3ab86531
Connie-hwang <hg.hwagn@pet-friends.co.kr>
2024-01-12 12:01:49 +0900

    refactor: 불필요한 주석 제거

--- todo-list/src/components/TodoList.jsx
+++ todo-list/src/components/TodoList.jsx
@@ -10,66 +10,6 @@ const ColumnGrid = styled.div`
 
 const TodoList = () => {
 
-    // const todos = [
-    //     {
-    //         id: 1,
-    //         title: '첫째날 할일',
-    //         date: '2024년 1월 11일 목요일',
-    //         tasks: [
-    //             {
-    //                 id: 1,
-    //                 task: "task1"
-    //             },
-    //             {
-    //                 id: 2,
-    //                 task: "task2"
-    //             },
-    //             {
-    //                 id: 3,
-    //                 task: "task2"
-    //             },
-    //         ]
-    //     },
-    //     {
-    //         id: 2,
-    //         title: '둘째날 할일',
-    //         date: '2024년 1월 12일 금요일',
-    //         tasks: [
-    //             {
-    //                 id: 1,
-    //                 task: "task1"
-    //             },
-    //             {
-    //                 id: 2,
-    //                 task: "task2"
-    //             },
-    //             {
-    //                 id: 3,
-    //                 task: "task2"
-    //             },
-    //         ]
-    //     },
-    //     {
-    //         id: 3,
-    //         title: '셋째날 할일',
-    //         date: '2024년 1월 13일 토요일',
-    //         tasks: [
-    //             {
-    //                 id: 1,
-    //                 task: "task1"
-    //             },
-    //             {
-    //                 id: 2,
-    //                 task: "task2"
-    //             },
-    //             {
-    //                 id: 3,
-    //                 task: "task2"
-    //             },
-    //         ]
-    //     },
-    // ];
-
     const apiUrl = 'http://localhost:8080/todos';
     const [todos, setData] = useState([]);
 
