9b1cc641a7f4fa5ab4754660e15c1b1c154ab613
hku2002 <hku2002@hanmail.net>
2024-01-15 22:14:44 +0900

    feat: 인피니티 스크롤 추가

--- product-list/src/components/ProductList.jsx
+++ product-list/src/components/ProductList.jsx
@@ -9,35 +9,65 @@ const ProductListContainer = styled.div`
   justify-content: center;
 `;
 
-const ProductList = () => {
+const ProductList = (callback, deps) => {
 
-    const [products, setProduct] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(0);
+    const [loading, setLoading] = useState(false);
     const apiUrl = "http://localhost:8080/products";
+    const size = 10;
+
 
     const fetchProducts = useCallback(async (page, size) => {
-
         const url = new URL(apiUrl);
         url.searchParams.append('page', page);
         url.searchParams.append('size', size);
 
         const response = await fetch(url.toString());
         const jsonData = await response.json();
-        console.log(jsonData);
-        setProduct(jsonData.data.content);
+        return jsonData.data.content;
     }, [apiUrl]);
 
+    const loadMore = useCallback(async () => {
+        if (loading) return;
+        setLoading(true);
+
+        try {
+            const newProducts = await fetchProducts(page + 1, size);
+            setProducts((prevProducts) => [...prevProducts, ...newProducts]);
+            setPage(page + 1);
+        } catch (error) {
+            console.error("Error fetching more products:", error);
+        } finally {
+            setLoading(false);
+        }
+    }, [loading, fetchProducts, page, size]);
+
+    const handleScroll = useCallback(async () => {
+        if (
+            window.innerHeight + document.documentElement.scrollTop ===
+            document.documentElement.offsetHeight
+        ) {
+            await loadMore();
+        }
+    }, [loadMore]);
+
+
     useEffect(() => {
-        console.log('use effect');
-        fetchProducts(0, 10);
-    }, [fetchProducts]);
+        fetchProducts(0, size).then((initialProducts) => setProducts(initialProducts));
+    }, [fetchProducts, size]);
 
-
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [handleScroll]);
 
     return (
         <ProductListContainer>
-            {products.map((product) => (
-                <ProductComponent key={product.id} {...product} />
+            {products.map((product, index) => (
+                <ProductComponent key={index} {...product} />
             ))}
+            {loading && <p>Loading...</p>}
         </ProductListContainer>
     );
 };
