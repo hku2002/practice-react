0d8018301dd412c53b80a056fee172c1c75e38c7
hku2002 <hku2002@hanmail.net>
2024-01-16 23:49:31 +0900

    feat: test 추가

--- todo-list/src/components/TodoTemplate.test.jsx
+++ todo-list/src/components/TodoTemplate.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, waitFor, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import TodoTemplate from './TodoTemplate';
 
@@ -24,6 +24,27 @@ describe('TodoTemplate Component Test', () => {
         // Task 목록이 렌더링되었는지 확인
         expect(screen.getByText('Task 1')).toBeInTheDocument();
         expect(screen.getByText('Task 2')).toBeInTheDocument();
+    });
+
+    it('Add Task 기능 작동 시 추가된 Task가 올바르게 렌더링 된다.', async () => {
+        render(
+            <TodoTemplate
+                todoId={1}
+                title="Sample Todo"
+                date="2022-01-01"
+                tasks={[
+                    { id: 1, task: 'Task 1', completed: false },
+                    { id: 2, task: 'Task 2', completed: true },
+                ]}
+            />
+        );
+
+        // 새로운 Task 추가
+        fireEvent.change(screen.getByRole('textbox'), { target: { value: 'New Task' } });
+        fireEvent.click(screen.getByText('Add Task'));
+
+        // 추가된 Task가 렌더링되었는지 확인
+        await waitFor(() => expect(screen.getByText('New Task')).toBeInTheDocument());
     });
 
 });
--- todo-list/src/mocks/handlers.js
+++ todo-list/src/mocks/handlers.js
@@ -3,16 +3,21 @@ export const handlers = [
     rest.get('http://localhost:8080/todos', (req, res, ctx) => {
         return res(
             ctx.status(200),
+            ctx.json([
+                { id: 1, task: 'Task 1', completed: false },
+                { id: 2, task: 'Task 2', completed: true },
+            ]),
+        );
+    }),
+    rest.post('http://localhost:8080/todo/task', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
             ctx.json({
-                status: 'success',
-                message: '',
-                data: {
-                    content: [
-                        { id: 1, task: 'Task 1', completed: false },
-                        { id: 2, task: 'Task 2', completed: true },
-                    ],
-                },
-            }),
+                    id: 3,
+                    task: 'New Task',
+                    completed: false,
+                }
+            ),
         );
     })
 ]
