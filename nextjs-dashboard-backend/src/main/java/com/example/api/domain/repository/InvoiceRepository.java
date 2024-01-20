bad1633be768021caec410d8fc65569965232534
hku2002 <hku2002@hanmail.net>
2024-01-20 23:10:43 +0900

    feat: 쿼리 추가

--- nextjs-dashboard-backend/src/main/java/com/example/api/domain/repository/InvoiceRepository.java
+++ nextjs-dashboard-backend/src/main/java/com/example/api/domain/repository/InvoiceRepository.java
@@ -3,6 +3,7 @@ package com.example.api.domain.repository;
 import com.example.api.domain.entity.Invoice;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,15 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     List<Invoice> findByCustomer_NameContainingIgnoreCaseOrCustomer_EmailContainingIgnoreCaseOrAmountContainingIgnoreCaseOrDateContainingIgnoreCaseOrStatusContainingIgnoreCase(
             String name, String email, String amount, String date, String status, Pageable pageable);
+
+    @Query(value = "SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id\n" +
+            "      FROM invoices\n" +
+            "      JOIN customers ON invoices.customer_id = customers.id\n" +
+            "      ORDER BY invoices.date DESC\n" +
+            "      LIMIT 5", nativeQuery = true)
+    List<Invoice> findLatestInvoice();
+
+    @Query(value = "SELECT COUNT(*) FROM invoices", nativeQuery = true)
+    int countInvoice();
 
 }
